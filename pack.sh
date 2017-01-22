caab3c7e11f9ca1730b732f8391bfbd4ef80e3e8
codl <codl@codl.fr>
2017-01-22 10:06:59 +0100

    Revert "base64 makes css less compressible so lets skip it"
    This reverts commit ef2d3697698cda7046efbd80c43365c26e75e0a8.
    
    turns out this confuses edge

--- pack.sh
+++ pack.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)
 
 icon="data:image/png;base64,$(base64 src/icon.png | tr -d "\n")"
-css="data:text/css,$(cat src/panel.css | sed "s~^\s\+~~" | tr -d "\n")"
+css="data:text/css;base64,$(cat src/panel.css | sed "s~^\s\+~~" | tr -d "\n" | base64 | tr -d "\n")"
 
 mkdir -p lib
 sed "s~icon\.png~$icon~;s~panel\.css~$css~" < src/nfig.js > lib/nfig.js

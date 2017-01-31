038170329ef49c5ccda69471d8c164210e7c06f8
codl <codl@codl.fr>
2017-02-01 00:14:29 +0100

    lets take the svg out of the js

--- pack.sh
+++ pack.sh
@@ -3,6 +3,7 @@ cd $(dirname $0)
 
 icon="data:image/png;base64,$(base64 src/icon.png | tr -d "\n")"
 css="data:text/css;base64,$(cat src/panel.css | sed "s~^\s\+~~" | tr -d "\n" | base64 | tr -d "\n")"
+svg="$(cat src/controller.svg | sed "s~^\s\+~~" | tr -d "\n")"
 
 mkdir -p lib
-sed "s~icon\.png~$icon~;s~panel\.css~$css~" < src/nfig.js > lib/nfig.js
+sed "s~icon\.png~$icon~;s~panel\.css~$css~;s~{{SVG}}~$svg~" < src/nfig.js > lib/nfig.js

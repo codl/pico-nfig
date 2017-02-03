29d330440d048598722e71c3b2abc905c56230ae
codl <codl@codl.fr>
2017-02-03 11:54:53 +0100

    separate bindings by version

--- pack.sh
+++ pack.sh
@@ -4,6 +4,7 @@ cd $(dirname $0)
 icon="data:image/png;base64,$(base64 src/icon.png | tr -d "\n")"
 css="data:text/css;base64,$(cat src/panel.css | sed "s~^\s\+~~" | tr -d "\n" | base64 | tr -d "\n")"
 svg="$(cat src/controller.svg | sed "s~^\s\+~~" | tr -d "\n")"
+major=$(git describe --tags --abbrev=0 | sed "s/\..*//")
 
 mkdir -p lib
-sed "s~icon\.png~$icon~;s~panel\.css~$css~;s~{{SVG}}~$svg~" < src/nfig.js > lib/nfig.js
+sed "s~icon\.png~$icon~;s~panel\.css~$css~;s~{{SVG}}~$svg~;s~{{MAJOR_VERSION}}~$major~" < src/nfig.js > lib/nfig.js
